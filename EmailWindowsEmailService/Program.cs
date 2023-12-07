1eda148105dd3cd02774866f6e46689ba82d5b06
Breno Narde Silva <brenonarde@gmail.com>
2023-12-07 17:47:45 -0300

    Add dbcontext scoped service no servico worker

--- EmailWindowsEmailService/Program.cs
+++ EmailWindowsEmailService/Program.cs
@@ -1,6 +1,12 @@
+using AppRepository.Data;
 using EmailWindowsEmailService;
+using Microsoft.EntityFrameworkCore;
 
 var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddDbContextPool<ApplicationContext>(options =>
+{
+    options.UseInMemoryDatabase("db");
+});
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
