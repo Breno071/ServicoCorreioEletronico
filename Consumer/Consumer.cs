833a924a9d2615f3ea30c1251ba35585b86e3911
Breno Narde Silva <brenonarde@gmail.com>
2023-12-11 11:45:41 -0300

    Ajustes no consumer

--- Consumer/Consumer.cs
+++ Consumer/Consumer.cs
@@ -11,6 +11,7 @@ using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Utills.Models;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace ConsumerWindowsService
 {
@@ -22,65 +23,78 @@ namespace ConsumerWindowsService
         };
         private const string QUEUE_NAME = "Adoptions";
         private readonly ApplicationContext _context;
+        private IModel channel;
+        private IConnection _connection;
 
         public Consumer(ApplicationContext context)
         {
             _context = context;
+            _connection = _factory.CreateConnection();
+            channel = _connection.CreateModel();
         }
 
         public async Task<List<AdoptRequest>> GetMessages()
         {
             List<AdoptRequest> messages = [];
-            return await Task.Run(() =>
+
+            using var connection = _factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: QUEUE_NAME,
+                        durable: true,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+            var tcs = new TaskCompletionSource<object>();
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
             {
-                using var connection = _factory.CreateConnection();
-                using var channel = connection.CreateModel();
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
 
-                channel.QueueDeclare(queue: QUEUE_NAME,
-                         durable: true,
-                         exclusive: false,
-                         autoDelete: false,
-                         arguments: null);
-
-
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (model, ea) =>
+                Console.Out.WriteLine(body.ToString());
+                Console.Out.WriteLine(message);
+                try
                 {
-                    var body = ea.Body.ToArray();
-                    var message = Encoding.UTF8.GetString(body);
-
-                    try
+                    if (!string.IsNullOrEmpty(message))
                     {
-                        if (!string.IsNullOrEmpty(message))
-                        {
-                            AdoptRequest request = JsonSerializer.Deserialize<AdoptRequest>(message);
+                        AdoptRequest request = JsonSerializer.Deserialize<AdoptRequest>(message);
 
-                            if (request is not null)                            
-                                messages.Add(request);
-                            else
-                                throw new Exception($"Não foi possível deserializar a mensagem {message}");
-                        }
+                        if (request is not null)
+                            messages.Add(request);
+                        else
+                            throw new Exception($"Não foi possível deserializar a mensagem {message}");
                     }
-                    catch( JsonException jsonEx)
-                    {
-                        ILogRepository logRepository = new LogRepository(_context);
-                        logRepository.Add(new Log()
-                        {
-                            LogType = LogType.Error,
-                            Message = jsonEx.Message
-                        });
-                    }
-                    catch (Exception)
-                    {
 
-                        throw;
-                    }
-                };
-                channel.BasicConsume(queue: QUEUE_NAME,
-                                      autoAck: true,
-                                      consumer: consumer);
-                return messages;
-            });
+                    //Informa que a mensagem foi consumida
+                    tcs.SetResult(true);
+                }
+                catch (JsonException jsonEx)
+                {
+                    ILogRepository logRepository = new LogRepository(_context);
+                    logRepository.Add(new Log()
+                    {
+                        LogType = LogType.Error,
+                        Message = jsonEx.Message
+                    });
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            };
+
+            channel.BasicConsume(queue: QUEUE_NAME,
+                                  autoAck: true,
+                                  consumer: consumer);
+
+            await Task.WhenAny(tcs.Task, Task.Delay(100000));
+
+
+            return messages;
         }
     }
 }
