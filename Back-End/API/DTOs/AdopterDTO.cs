d8ebd2ec4bf745db0c41300b6bbe4b592b34e304
Breno Narde Silva <brenonarde@gmail.com>
2023-12-12 09:19:48 -0300

    Adição de DTOs

---
+++ Back-End/API/DTOs/AdopterDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace API.DTOs
+{
+    public class AdopterDTO
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string CPF { get; set; } = string.Empty;
+        public DateTime DataNascimento { get; set; }
+    }
+}
---
+++ Back-End/API/DTOs/AdopterRequestDTO.cs
@@ -0,0 +1,12 @@
+﻿using AppRepository.Entities;
+using AppRepository.Enums;
+
+namespace API.DTOs
+{
+    public class AdopterRequestDTO
+    {
+        public AdopterDTO Adopter { get; set; } = new();
+        public string Breed { get; set; } = string.Empty;
+        public AnimalType AnimalType { get; set; }
+    }
+}
