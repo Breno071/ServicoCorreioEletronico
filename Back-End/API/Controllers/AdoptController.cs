eed6f7b0cd9d0b3d201e6e344b5479a15e100d54
Breno Narde Silva <brenonarde@gmail.com>
2023-12-14 10:57:52 -0300

    Adiciona listagem de pets e consulta de detalhes

--- Back-End/API/Controllers/AdoptController.cs
+++ Back-End/API/Controllers/AdoptController.cs
@@ -18,33 +18,54 @@ namespace API.Controllers
         }
 
         [HttpPost("adopt")]
-        public async Task<IActionResult> Adopt([FromBody] AdopterRequestDTO adoption)
+        public async Task<IActionResult> Adopt([FromBody] AdopterRequestDTO adoptionRequest)
         {
-            if (!Validations.IsEmailValid(adoption.Adopter.Email))
+            if (!Validations.IsEmailValid(adoptionRequest.Adopter.Email))
                 return BadRequest("E-mail inválido!");
 
-            if(!Validations.IsCpfValid(adoption.Adopter.CPF))
+            if(!Validations.IsCpfValid(adoptionRequest.Adopter.CPF))
                 return BadRequest("CPF inválido!");
 
-            AdoptRequest request = new()
+            AdoptRequest adoption = new()
             {
                 Adopter = new Adopter()
                 {
-                    CPF = adoption.Adopter.CPF,
-                    Email = adoption.Adopter.Email,
-                    Nome = adoption.Adopter.Nome,
-                    DataNascimento = adoption.Adopter.DataNascimento
+                    CPF = adoptionRequest.Adopter.CPF,
+                    Email = adoptionRequest.Adopter.Email,
+                    Nome = adoptionRequest.Adopter.Nome,
+                    DataNascimento = adoptionRequest.Adopter.DataNascimento
                 },
-                Breed = adoption.Breed,
-                AnimalType = adoption.AnimalType
+                Breed = adoptionRequest.Breed,
+                AnimalType = adoptionRequest.AnimalType
             };
 
             await Task.Run(() =>
             {
-                _producer.Send(request);
+                _producer.Send(adoption);
             });
 
-            return Accepted(request);
+            return Accepted(adoptionRequest);
+        }
+
+        [HttpGet("list-pets")]
+        public async Task<IActionResult> ListPets(string specie, string age, char sex)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, $"https://api-staging.adoptapet.com/search/pet_search?key=hg4nsv85lppeoqqixy3tnlt3k8lj6o0c&v=3&output=json&city_or_zip=47374&geo_range=50&species={specie.ToLower()}&breed_id=real=801&sex={sex}&age={age}&color_id=54&pet_size_range_id=2&hair=&bonded_pair=&special_needs=&include_mixes=&added_after=&start_number=1&end_number=50&meta_only=0");
+            request.Headers.Add("Accept", "application/json");
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            return Ok(await response.Content.ReadFromJsonAsync<object>());
+        }
+
+        [HttpGet("datails/{pet_id:long}")]
+        public async Task<IActionResult> GetPetDetails(long pet_id)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, $"https://api-staging.adoptapet.com/search/pet_details?pet_id={pet_id}&key=hg4nsv85lppeoqqixy3tnlt3k8lj6o0c&v=3&output=json");
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            return Ok(await response.Content.ReadFromJsonAsync<object>());
         }
     }
 }
