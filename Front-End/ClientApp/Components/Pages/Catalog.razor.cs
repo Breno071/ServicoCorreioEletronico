e8a182da8608a5e8f9cd7c6b6f0da54387403869
Breno Narde Silva <brenonarde@gmail.com>
2023-12-19 14:03:39 -0300

    add css inicial para o catalogo e rendermode InteractiveServer

---
+++ Front-End/ClientApp/Components/Pages/Catalog.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+
+namespace ClientApp.Components.Pages
+{
+    public partial class Catalog
+    {
+        //private IJSObjectReference module;
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await JS.InvokeAsync<IJSObjectReference>("import", "./Components/Pages/Catalog.razor.js");
+            }
+        }
+    }
+}
---
+++ Front-End/ClientApp/Components/Pages/Catalog.razor.js
@@ -0,0 +1,11 @@
+﻿$(".product-colors span").click(function () {
+    $(".product-colors span").removeClass("active");
+    $(this).addClass("active");
+    $(".active").css("border-color", $(this).attr("data-color-sec"))
+    /*$("#catalog").css("background", $(this).attr("data-color-primary"));*/
+    $(".content h2").css("color", $(this).attr("data-color-sec"));
+    $(".content h3").css("color", $(this).attr("data-color-sec"));
+    $(".container .imgBx").css("background", $(this).attr("data-color-sec"));
+    $(".container .details button").css("background", $(this).attr("data-color-sec"));
+    $(".imgBx img").attr('src', $(this).attr("data-pic"));
+});
