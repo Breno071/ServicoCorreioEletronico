8acc5769f5055cef01c56e216ae29f2af8ce8947
Breno Narde Silva <brenonarde@gmail.com>
2023-12-19 19:16:52 -0300

    Mais ajustes no carrocel

--- Front-End/ClientApp/Components/Pages/Catalog.razor.cs
+++ Front-End/ClientApp/Components/Pages/Catalog.razor.cs
@@ -1,16 +1,50 @@
-﻿using Microsoft.JSInterop;
+﻿using ClientApp.Models;
+using Microsoft.JSInterop;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace ClientApp.Components.Pages
 {
     public partial class Catalog
     {
-        //private IJSObjectReference module;
+        private Request Request = new();
+        private bool Loading = false;
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             if (firstRender)
             {
                 await JS.InvokeAsync<IJSObjectReference>("import", "./Components/Pages/Catalog.razor.js");
             }
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Loading = true;
+            using var client = new HttpClient();
+            using var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:5145/api/Adopt/list-pets?specie=dog&age=young&sex=m");
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            Request = JsonSerializer.Deserialize<Request>(await response.Content.ReadAsStringAsync()) ?? throw new Exception("Falha ao deserializar conteudo do json");
+
+            Request.pets = Request.pets.OrderByDescending(p => p.pet_id).ToList();
+            Loading = false;
+        }
+
+        //Metodo para verificar se a imagem recebida pode ser carregada
+        private bool GetImage(string url)
+        {
+            using var cliente = new HttpClient();
+            using var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var response = cliente.Send(request);
+            try
+            {
+                response.EnsureSuccessStatusCode();
+            }
+            catch
+            {
+                return false;
+            }
+            return response.IsSuccessStatusCode;
         }
     }
 }
---
+++ Front-End/ClientApp/Models/Request.cs
@@ -0,0 +1,29 @@
+﻿namespace ClientApp.Models
+{
+    public class Pet
+    {
+        public string sex { get; set; } = string.Empty;
+        public string age { get; set; } = string.Empty;
+        public string size { get; set; } = string.Empty;
+        public int order { get; set; }
+        public string pet_id { get; set; } = string.Empty;
+        public string pet_name { get; set; } = string.Empty;
+        public string primary_breed { get; set; } = string.Empty;
+        public string secondary_breed { get; set; } = string.Empty;
+        public string addr_city { get; set; } = string.Empty;
+        public string addr_state_code { get; set; } = string.Empty;
+        public string details_url { get; set; } = string.Empty;
+        public string results_photo_url { get; set; } = string.Empty;
+        public int results_photo_width { get; set; }
+        public int results_photo_height { get; set; }
+        public string large_results_photo_url { get; set; } = string.Empty;
+        public int large_results_photo_width { get; set; }
+        public int large_results_photo_height { get; set; }
+    }
+
+    public class Request
+    {
+        public string status { get; set; } = string.Empty;
+        public List<Pet> pets { get; set; } = new();
+    }
+}
